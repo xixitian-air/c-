3937e62245673aa86ad8add65760e82af199f548
xixitian-air <2464103352@qq.com>
2020-12-28 19:49:03 +0800

    commit 逆向输出字符2

---
+++ 逆向输出字符2.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+ int b[2000];
+ b[0]=0;
+ char a[2000];
+ fgets(a,2000,stdin);
+ int len,k=0;
+ len =strlen(a);
+ for(int i=0;i<len;i++)
+ {
+ if(a[i]==' ')
+ {
+ b[k+1]=i;	
+ k++;		
+ }	
+ }
+ b[k+1]=len;
+ for(int j=0;j<k+1;j++)
+ {
+ for(int h = b[j+1];h>b[j];h--)
+ {
+ printf("%c",a[h-1]);	 	
+ if(j==0&&h==b[j]+1)
+ printf(" ");
+ }    
+ }
+return 0;
+}
+
